799dd13d515363a44aace5ca96f8ec2b90c48af5
Brian Mason <bpmason1@hotmail.com>
2020-12-14 19:13:10 -0500

    add tupe annotations to ProgramNode class

--- src/builder/ProgramNode.py
+++ src/builder/ProgramNode.py
@@ -9,13 +9,13 @@ class __ProgramNode(AstNode):
     def llvmIR(self):
         None
 
-    def newPackage(self, name):
+    def newPackage(self, name: str) -> PackageNode:
         if self.getPackage(name):
             raise Exception(f'ERROR: Package {name} already exists')
 
         return PackageNode(name, parent=self)
 
-    def getPackage(self, name):
+    def getPackage(self, name: str) -> PackageNode:
         for node in self.children:
             if node.name == name:
                 return node
