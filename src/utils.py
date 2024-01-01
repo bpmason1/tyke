edb582c74d91ba868b6862868cd97476da383605
Brian Mason <bpmason1@hotmail.com>
2024-01-01 09:21:01 -0500

    colorama Fore.Red should be Fore.RED

--- src/utils.py
+++ src/utils.py
@@ -5,5 +5,5 @@ def warn(msg):
     print(Fore.YELLOW + Style.BRIGHT + msg + Style.RESET_ALL)
 
 def fail_fast(msg, exit_code=66):
-    sys.stderr.write(Fore.Red + msg + Style.RESET_ALL + '\n')
+    sys.stderr.write(Fore.RED + msg + Style.RESET_ALL + '\n')
     sys.exit(exit_code)
